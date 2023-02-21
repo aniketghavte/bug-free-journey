2a7d0a5987635cb0eeae032b71d0e20b99bfa3ea
aniketghavte <104782783+aniketghavte@users.noreply.github.com>
2023-02-22 01:45:09 +0530

    First Deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,35 +3,84 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import axios from 'axios';
 
 const inter = Inter({ subsets: ['latin'] })
 
+
 export default function Home() {
 
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+  const [Name, setName] = useState("");
+  const [Department, setDepartment] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
+  const [category, setCategory] = useState("");
+  const [option,setOption] = useState()
 
+
+  const [isRegister, setIsRegister] = useState(false);
+
+  const CreateUser = () => {
+    // axios.post('https://script.google.com/macros/s/AKfycbxAaMWhgFXgYX3Plxs0nzNXGcORBMFtUiCY9GJ1SpjVJDzXT3vCtznPs_L96ijGf18T/exec', {
+    //   Name: "dummy",
+    //   Email: "dummy email",
+    //   Message: "dummy message"
+    // })
+    // .then(function (response) {
+    //   console.log(response);
+    // })
+    // .catch(function (error) {
+    //   console.log(error);
+    // });
+    // let headers = new Headers();
+    // headers.append('Content-Type', 'application/json');
+    // headers.append('Accept', 'application/json');
+    // headers.append('Origin','http://localhost:3001');
+
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbyehsmmRy-kceWsemHunOO43hADGha2zPxSNMv4hT4RSWCGSZqlxCxCGWBybwVLalpU/exec'
+    const form = document.forms['submit-to-google-sheet']
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response =>{
+          console.log(response)
+          setIsRegister(true);
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    })
+
+  }
+
+  function handleChange(event){
+    setOption(event.target.value)
+    console.log(option)
+
+}
+
+
+ 
   const handleSubmit = (event) => {
   event.preventDefault();
-
-  if (firstName === "") {
-      toast.error("Enter first Name");
-  } else if (lastName === "") {
-      toast.error("Enter Last Name")
-  } else if(phone > 10000000000 || phone < 999999999) {
-      toast.error("Enter Valid Mobile no")
-  }else if(email === ""){
-      toast.error("Enter email")
-  }else{
-      Data.firstName = firstName;
-      Data.lastName = lastName;
-      Data.email = email;
-      Data.phone = Number(phone) ;
+  CreateUser()
+  // if (firstName === "") {
+  //     toast.error("Enter first Name");
+  // } else if (lastName === "") {
+  //     toast.error("Enter Last Name")
+  // } else if(phone > 10000000000 || phone < 999999999) {
+  //     toast.error("Enter Valid Mobile no")
+  // }else if(email === ""){
+  //     toast.error("Enter email")
+  // }else{
+  //     Data.firstName = firstName;
+  //     Data.lastName = lastName;
+  //     Data.email = email;
+  //     Data.phone = Number(phone) ;
   
-      CreateUser()
-  }
+  //     CreateUser()
+  // }
   
   }
   return (
@@ -48,43 +97,122 @@ export default function Home() {
 
 
       <div className={styles.center}>
+          <div>
+            <p style={{fontSize: '1.5rem'}}>Department of Information Technology</p>
+            <p style={{fontSize: '2.5rem', marginTop: '2rem', fontWeight: '600'}}>ITSA</p>
+            <p style={{fontSize: '1.5rem', marginTop: '1rem'}}>Presents</p>
+            <div className={styles.thirteen} style={{marginTop: '2rem', marginLeft: '4rem'}}>
+            <p style={{fontSize: '2rem', fontWeight: '600'}}>DPU's Got Talent</p>
+             </div>
+             {/* <Image
+                src="/dancingguy.svg"
+                alt="Vercel Logo"
+                width={100}
+                height={100}
+                priority
+             /> */}
+             <div style={{marginTop: '3rem', fontSize: '1.3rem'}}>
+                <p style={{marginTop: '0.5rem'}}>Experience </p>
+                <p style={{marginTop: '0.5rem'}}>The</p>
+                <p style={{marginTop: '0.5rem'}}>Amazing Creativity</p>
+             </div>
+
+          </div>
           
-          <div className={styles.thirteen}>
-            <p>ITSA</p>
-          </div>
         </div>
-        {/* <div className='app__form'>
 
-          <div className='form__header'>
-              <p className='step__count'><span style={{color: '#ddab6c'}}>1</span>/4</p>
-              <p className='form__name'>Create Account</p> 
+
+
+        <div className={styles.app__form}>
+
+          {!isRegister ? <>
+              <div>
+                <div className='form__header' style={{display: 'flex' , flexDirection: 'column', textAlign: 'center'}}>
+                    <p style={{color: '#ddab6c', fontSize: '2rem'}}>ITSA DPU</p>
+                    <p className='form__name' style={{fontSize: '1.5rem',marginTop: '1rem'}}>Register Here !!!!</p> 
+                    <p className='form__name' style={{fontSize: '1.5rem',marginTop: '0.5rem'}}>Fill the Details Below to Register for the Event</p> 
+                </div>
+
+                <form onSubmit={handleSubmit} name="submit-to-google-sheet" style={{marginTop: '8rem'}}>
+                    <div className={styles.form__item} >            
+                        <p style={{textAlign: 'left'}}>Name</p>
+                        <input className={styles.form__input} type="text" value={Name} name="Name" onChange={(e) => setName(e.target.value)}/>
+                    </div>
+
+                    <div className={styles.form__item}>            
+                        <p  style={{textAlign: 'left'}}>Email</p>
+                        <input className={styles.form__input} type="email" value={email} name="Email" onChange={(e) => setEmail(e.target.value)}/>
+                    </div>
+                    <div className={styles.form__item}>            
+                        <p  style={{textAlign: 'left'}}>Phone</p>
+                        <input className={styles.form__input} type="tel" value={phone} name="Phone" onChange={(e) => setPhone(e.target.value)}/>
+                    </div>
+                    <div className={styles.form__item}>            
+                        <p  style={{textAlign: 'left'}}>Department and Year</p>
+                        <input className={styles.form__input} type="text" value={Department} name="Department" onChange={(e) => setDepartment(e.target.value)}/>
+                    </div>
+                    {/* <div className={styles.form__item}>            
+                        <p  style={{textAlign: 'left'}}></p>
+                        <label for="cars">Choose a car:</label>
+
+                          <select name="cars" id="cars" onChange={handleChange}>
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            <option value="mercedes">Mercedes</option>
+                            <option value="audi">Audi</option>
+                          </select>
+                    </div> */}
+                    <div className={styles.form__item}>  
+                    <div style={{textAlign: 'left'}}>
+                      <p>Choose any one Category : </p>
+                      <p style={{marginTop: '1rem'}}> 𝘿𝙖𝙣𝙘𝙞𝙣𝙜 (Gr/Solo)</p>
+                      <p> 🎤𝙎𝙞𝙣𝙜𝙞𝙣𝙜 (Gr/Solo) </p>
+                      <p> 🖊️𝙋𝙤𝙚𝙩𝙧𝙮</p>
+                      <p> 🎨𝘿𝙧𝙖𝙬𝙞𝙣𝙜/𝙋𝙖𝙞𝙣𝙩𝙞𝙣𝙜</p>
+                      <p> 🎨🎭𝘾𝙤𝙢𝙚𝙙𝙮 𝙎𝙝𝙤𝙬</p>
+                      <p> 🎹 𝙄𝙣𝙨𝙩𝙧𝙪𝙢𝙚𝙣𝙩 𝙋𝙡𝙖𝙮</p>
+                      <p> 🪂𝙁𝙖𝙨𝙝𝙞𝙤𝙣 𝙎𝙝𝙤𝙬</p>
+                      <p>  🎬𝙎𝙠𝙞𝙩 </p>
+                      </div>         
+                      
+                        <input className={styles.form__input} type="text" value={category} name="Category" onChange={(e) => setCategory(e.target.value)}/>
+                    </div>
+
+                    <div className={styles.form__submit}>
+                        {/* <Image
+                              src="/dancingguy.svg"
+                              alt="Vercel Logo"
+                              width={100}
+                              height={100}
+                              priority
+                          /> */}
+                        <input style={{fontSize: '1.4rem'}}  type="submit" value='Register'/>
+                    </div>
+
+                </form>
+              </div>
+          </> : <>
+           <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+            <div >
+               <p style={{textAlign:'center'}}>We Got Your Response</p>
+               <p style={{textAlign:'center'}}>Thanks For Registration  </p>
+            </div>
+            <div style={{display: 'flex', alignItems:'center', marginTop: '10%'}}>
+              <Image
+                  src="/instagram.gif"
+                  alt="Vercel Logo"
+                  width={100}
+                  height={100}
+                  priority
+                />
+                <p style={{marginLeft: '1rem'}}><a href='https://www.instagram.com/itsa_dpu/?hl=en' target='_blank'> Click here to Follow us on Instagarm</a></p>
+            </div>
+           </div>
+          </>}
+
+
+
           </div>
-
-          <form onSubmit={handleSubmit}>
-              <div className='form__item'>            
-                  <p>First Name</p>
-                  <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
-              </div>
-              <div className='form__item'>            
-                  <p>Last Name</p>
-                  <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
-              </div>
-              <div className='form__item'>            
-                  <p>Email</p>
-                  <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-              </div>
-              <div className='form__item'>            
-                  <p>Phone</p>
-                  <input type="tel" value={phone} onChange={(e) => setPhone(e.target.value)}/>
-              </div>
-
-
-              <div className='form__submit'>
-                  <input type="submit" value='Create Account'/>
-              </div>
-
-          </form>
-          </div> */}
 
         {/* <div className={styles.description}>
           <p>
